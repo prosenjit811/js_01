77aab25069894551847a0881ed16df355d5de8e0
Prosenjit Paul <164428158+prosenjit811@users.noreply.github.com>
2024-08-08 16:12:43 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*example01 start*/
+function showText() {
+	document.getElementById('s_text').innerHTML="Hello world";
+}
+/*example01 end*/
+
+/*example 02 start*/
+function showParagraph(){
+	document.querySelector('#s_par').innerHTML="In publishing and graphic design,Lorem ipsum";
+}
+/*example 02 end*/
+
+/*example 03 start*/
+function Para_change(){
+	document.querySelector('#Para_change').innerHTML="Change the paragraph text";
+}
+/*example 03 end*/
+
+/*example 04 start*/
+function color_change(){
+	document.querySelector('#color_change').style.color="red";
+}
+/*example 04 end*/
+
+/*example 05 start*/
+function font_size(){
+	document.querySelector('#font_size').style.fontSize="30px";
+}
+/*example 05 end*/
